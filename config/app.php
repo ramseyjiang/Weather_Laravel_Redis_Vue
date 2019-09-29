2895a132b7da3566f6bd6ca4728399279616c6a2
姜大巍 <davy820ramsey@gmail.com>
2019-09-29 21:12:16 +1300

    Update namespace and structure.

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Pacific/Auckland',
 
     /*
     |--------------------------------------------------------------------------
@@ -169,11 +169,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        Weather\Providers\AppServiceProvider::class,
+        Weather\Providers\AuthServiceProvider::class,
+        // Weather\Providers\BroadcastServiceProvider::class,
+        Weather\Providers\EventServiceProvider::class,
+        Weather\Providers\RouteServiceProvider::class,
 
     ],
 
